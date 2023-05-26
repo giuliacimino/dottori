90565fcc0e36a4da6b99950e8db6be53287f4781
giuliacimino <giuliacimino00@gmail.com>
2023-05-26 15:35:06 +0200

    inizio progetto

---
+++ src/test/java/it/prova/dottori/DottoriApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.dottori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DottoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
